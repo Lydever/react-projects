9e269984ae57e47f95424084e17f9dba0b9c586e
Lydev68 <18027118545@163.com>
2022-01-16 23:39:01 +0800

    feat:react-router-dom v5升级为v6

---
+++ vite-react-app/src/router/index.js
@@ -0,0 +1,21 @@
+/**
+ * @Description:
+ * @Author:   liyingxia
+ * @CreateDate:  2022/1/16 22:53
+ */
+
+import Index from "../component/Index";
+import About from "../component/About";
+
+const routes = [
+  {
+    path: '/',
+    component: Index
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+
+export default routes;
