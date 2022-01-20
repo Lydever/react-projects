3b33bfa263d61e1dc99337ec604982237f89e54d
Lydev68 <18027118545@163.com>
2022-01-20 23:31:21 +0800

    feat:jsx之this绑定问题

--- vite-react-app/src/views/this绑定问题.jsx
+++ vite-react-app/src/views/this绑定问题.jsx
@@ -5,3 +5,82 @@
     - 原因是this在这里是undefined
   - 如果我们这里直接打印this，也会发现它是一个undefined
  */
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '例如按'
+    }
+    this.btnClick = this.btnClick.bind(this)
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.btnClick.bind(this)}></button>
+        /**
+        * 方案一：bind给btnClick显示绑定this
+        在传入函数时，我们可以主动绑定this：
+        这里我们主动将btnClick中的this通过bind来进行绑定（显示绑定）
+        那么之后React内部调用btnClick函数时，就会有一个this，并且是我们绑定的this；
+        */
+        button
+      </div>
+    )
+  }
+
+
+
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "你好啊,李银河"
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.btnClick}>点我一下(React)</button>
+        <button onClick={this.btnClick}>也点我一下(React)</button>
+      </div>
+    )
+  }
+
+  btnClick = () => {
+    console.log(this);
+    console.log(this.state.message);
+  }
+}
+
+/**
+ * 方案三： 事件监听传入箭头函数(推荐)
+ * 因为onClick中要求传入一个箭头函数，那么我们可以直接定义一个箭头函数传入：
+ *   - 传入的箭头函数的函数体是我们需要执行的代码，我们直接执行this.btnClick();
+ *   - this.btnClick()中通过this来指定会隐式绑定，最终this也是正确的
+ *
+ */
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'dsjdsd'
+    }
+  }
+  render() {
+    return (
+      <div>
+      <button onClick={() =>this.btnClick}>点我</button>
+      <button onClick={() =>this.btnClick}>点我</button>
+      </div>
+    )
+  }
+  btnClick() {
+    console.log(this);
+    console.log(this.state.message)
+  }
+}
